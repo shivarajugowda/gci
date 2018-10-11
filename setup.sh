9b132beb079d0f74ce0d977c8031deeaba460b35
Shiv Gowda <shivaraju.gowda@gmail.com>
2018-10-11 05:48:20 +0000

    tweak

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/fastai/fastai.git
 mkdir ~/data
-ln -s ~/data ~/fast.ai/fastai/courses/dl1/
+ln -s ~/data ~/fastai/courses/dl1/
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
