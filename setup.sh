58fc3a08be79baee2a392b569ae33e0994a431ab
Shiv Gowda <shivaraju.gowda@gmail.com>
2018-10-10 22:23:31 -0700

    setup fastai

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-Hello World	
+git clone https://github.com/fastai/fastai.git
+mkdir ~/data
+ln -s ~/data ~/fast.ai/fastai/courses/dl1/
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
